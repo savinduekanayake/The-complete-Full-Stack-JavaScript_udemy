804b9de95bde22a9504451308d0d4cf122cccaa8
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-03-19 00:42:14 +0530

    start routing

---
+++ website/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import PageWrapper from './components/PageWrapper';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Router>
+      <PageWrapper >
+
+        <Route
+          path="/"
+          component={Home}
+        />
+      </PageWrapper>
+    </Router>
+  );
+}
+
+export default App;
