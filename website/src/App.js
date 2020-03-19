ceb68f454df46e810d114f4a61045e9cc7dd9414
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-03-19 17:57:48 +0530

    routing

--- website/src/App.js
+++ website/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-import PageWrapper from './components/PageWrapper';
-import Home from './components/pages/Home';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+//pages
+import PageWrapper from './components/PageWrapper';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 function App() {
   return (
@@ -11,9 +13,20 @@ function App() {
       <PageWrapper >
 
         <Route
+        exact = {true}
           path="/"
           component={Home}
         />
+
+      <Route 
+        path = '/about'
+        component={About}
+        />
+
+        
+
+
+
       </PageWrapper>
     </Router>
   );
