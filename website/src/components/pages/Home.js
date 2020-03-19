ac4484067804b12c15c403f32e9018b96b284727
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-03-20 00:17:15 +0530

    services and portfolio done

--- website/src/components/pages/Home.js
+++ website/src/components/pages/Home.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import Header from '../Common/Header';
 import image from '../assets/img/header-bg.jpg';
 
+
+// Re-usable components
+import Services from '../Common/Services'; 
+import Portfolio from '../Common/portfolio';
+
+
 class Home extends Component {
 
     render(){
@@ -15,6 +21,9 @@ class Home extends Component {
                     showButton= {true}
                     image = {image}
                />
+
+               <Services />
+               <Portfolio />
             </div>
         )
     }
